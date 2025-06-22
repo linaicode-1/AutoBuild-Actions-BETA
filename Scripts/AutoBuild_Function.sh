a7a81210c6439c9c7fe8c6bb989d27f1ac005228
linai081795 <linai081795@gmail.com>
2025-06-22 13:14:12 +0800

    fix

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -208,6 +208,34 @@ EOF
 			sed -i "s/${Old_IP}/${Default_IP}/g" ${BASE_FILES}/bin/config_generate
 		fi
 	fi
+
+	if [[ "${DNS_Settings}" == "0" ]] || [[ -z "${DNS_Settings}" ]]; then
+		echo "不进行,DNS设置"
+	elif [[ -n "${DNS_Settings}" ]]; then
+		ipa_dns="$(echo ${DNS_Settings} |grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")"
+		if [[ -n "${ipa_dns}" ]]; then
+			sed -i "$lan\set network.lan.dns='${DNS_Settings}'" "${GENE_PATH}"
+			echo "DNS[${DNS_Settings}]设置完成"
+		else
+			TIME r "因DNS获取有错误，DNS设置失败，请检查DNS是否填写正确"
+		fi
+	fi
+
+	if [[ "${Disable_DHCP}" == "1" ]]; then
+		sed -i "$lan\set dhcp.lan.ignore='1'" "${GENE_PATH}"
+		echo "关闭DHCP设置完成"
+	else
+		echo "不进行,关闭DHCP设置"
+	fi
+
+	if [[ "${Cancel_running}" == "1" ]]; then
+		echo "sed -i '/coremark/d' /etc/crontabs/root" >> "${DEFAULT_PATH}"
+		echo "删除每天跑分任务完成"
+	else
+		echo "不进行,删除每天跑分任务"
+	fi
+
+    sed -i "$lan\set dhcp.lan.ignore='1'" ${BASE_FILES}/bin/config_generate
  	echo -e "### VARIABLE LIST ###\n$(cat ${GITHUB_ENV})\n"
 	ECHO "[Firmware_Diy_Main] Done"
 }
